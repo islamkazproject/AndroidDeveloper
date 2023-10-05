6f0d4089adedfa6ad4189860dd8029b2f7df6dab
Islam Mukanov <Islam.Mukanov@waveaccess.global>
2023-10-05 23:51:46 +0300

    Update GreetingPreview func: Added my name

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GreetingCardTheme {
-        Greeting("Android")
+        Greeting("Islam")
     }
 }
