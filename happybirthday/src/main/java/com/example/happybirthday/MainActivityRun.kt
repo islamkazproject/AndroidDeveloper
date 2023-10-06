a26b8bd4829d17f131f0e1f2ccf6e2ec837eb084
Islam Mukanov <Islam.Mukanov@waveaccess.global>
2023-10-06 11:45:32 +0300

    Added Alignment

--- happybirthday/src/main/java/com/example/happybirthday/MainActivityRun.kt
+++ happybirthday/src/main/java/com/example/happybirthday/MainActivityRun.kt
@@ -3,16 +3,20 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.GreetingCardTheme
 
@@ -35,7 +39,7 @@ class MainActivityRun : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    Column(verticalArrangement = Arrangement.Center, modifier = modifier) {
         Text(
             text = message,
             fontSize = 100.sp,
@@ -44,7 +48,10 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
         )
         Text(
             text = from,
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
         )
     }
 
