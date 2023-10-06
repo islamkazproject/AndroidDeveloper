d19a5ebd85225d6d1fc4f3f341cf46bda7b04d48
Islam Mukanov <Islam.Mukanov@waveaccess.global>
2023-10-06 12:33:26 +0300

    Added ContactList func

--- businesscard/src/main/java/com/example/businesscard/MainActivityBusinessCard.kt
+++ businesscard/src/main/java/com/example/businesscard/MainActivityBusinessCard.kt
@@ -3,12 +3,23 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.businesscard.ui.theme.GreetingCardTheme
 
@@ -30,17 +41,37 @@ class MainActivityBusinessCard : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ContactList() {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .background(Color(0xFFF6EDFF)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+
+    {
+        Row(modifier = Modifier.fillMaxWidth()){
+            val image = painterResource(R.drawable.ic_launcher_foreground)
+            Image(painter = image, contentDescription = null)
+            Text( text = "1")
+        }
+        Row(){
+            val image = painterResource(R.drawable.ic_launcher_foreground)
+            Image(painter = image, contentDescription = null);
+            Text( text = "2")
+        }
+        Row(){
+            val image = painterResource(R.drawable.ic_launcher_foreground)
+            Image(painter = image, contentDescription = null)
+            Text( text = "3")
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GreetingCardTheme {
-        Greeting("Android")
     }
 }
