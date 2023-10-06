d80fff7f5ba3f49d731bc72e3a56a93b1be23b2d
Islam Mukanov <Islam.Mukanov@waveaccess.global>
2023-10-06 12:50:42 +0300

    Optimized imports

--- businesscard/src/main/java/com/example/businesscard/MainActivityBusinessCard.kt
+++ businesscard/src/main/java/com/example/businesscard/MainActivityBusinessCard.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
