79542d95aaf4774695a29b19e06b52247b653b7f
Islam Mukanov <Islam.Mukanov@waveaccess.global>
2023-10-06 12:42:10 +0300

    Added ImageAuthor func

--- businesscard/src/main/java/com/example/businesscard/MainActivityBusinessCard.kt
+++ businesscard/src/main/java/com/example/businesscard/MainActivityBusinessCard.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,8 +21,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.GreetingCardTheme
 
 class MainActivityBusinessCard : ComponentActivity() {
@@ -33,12 +39,40 @@ class MainActivityBusinessCard : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    ContactList()
                 }
             }
         }
     }
 }
+
+@Composable
+fun ImageAuthor() {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .background(Color(0xFFFFFFFF)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        val image = painterResource(R.drawable.user)
+        val phone = painterResource(R.drawable.phone)
+        val nn = painterResource(R.drawable.tg)
+        val email = painterResource(R.drawable.emai)
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.height(150.dp))
+        Text(
+            text = stringResource(R.string.my_name),
+            modifier = Modifier.padding(top = 34.dp, bottom = 8.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(R.string.descr),
+            fontSize = 16.sp
+        )
 
 @Composable
 fun ContactList() {
